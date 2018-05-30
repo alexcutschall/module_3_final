652ff1465ce42e65461ca510551dc9f0cc25dc2c
alexcutschall <alexander.cutschall@gmail.com>
2018-05-30 11:50:28 -0600

    adding in a test for the last test

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -2,11 +2,12 @@ module Api
   module V1
     class PlaysController < ApplicationController
       def create
+        binding.pry
         game = Game.find(params[:game_id])
-        @play = game.plays.create(user_id: params[:player_id], word: params[:word])
+        play = game.plays.create(user_id: params[:player_id], word: params[:word])
         game.save
 
-        redirect_to api_v1_game_path(game), status: :created
+        redirect_to root_path
       end
     end
   end
---
+++ spec/requests/user_cannot_submit_an_invalid_word_to_play_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helpers'
+
+describe 'as a user' do
+  context 'when they post to a play' do
+    feature "if the word is invalid" do
+      it "doesn't play it" do
+        player_1, player_2 = create_list(:user, 2)
+        game = create(:game)
+        score_1 = create(:play, game: game, user: player_1, word: "hello")
+        word = "foxez"
+
+        params = {
+          "player_id" => player_2.id,
+          "word" => word
+        }
+
+        post "/api/v1/games/#{game.id}/plays", params: params
+
+        expect(message).to eq("foxez is not a valid word.")
+      end
+    end
+  end
+end
