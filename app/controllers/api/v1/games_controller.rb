aac1a98bfaa5e35877bbc3383ae6af1e46f59be9
alexcutschall <alexander.cutschall@gmail.com>
2018-05-30 10:33:46 -0600

    working on the game creation

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     class GamesController < ApplicationController
       def show
-        @game = GameSearch.new(params[:id]).find_game
+        @game = Game.find(params[:id])
+        binding.pry
+        render json: @game
       end
     end
   end
--- spec/requests/user_can_look_at_game_history_spec.rb
+++ spec/requests/user_can_look_at_game_history_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe 'as an unregistered user' do
   feature 'when they visit a game path' do
     it 'shows them that games information' do
+      player_1, player_2 = create_list(:p)
+      game = create(:game)
+
       get "/api/v1/games/1"
 
       game = JSON.parse(response.body)
