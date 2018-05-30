e2d0d97bcf18b1b3da18669ffaf54bf8099eb939
alexcutschall <alexander.cutschall@gmail.com>
2018-05-30 10:06:57 -0600

    getting the second user story set up

---
+++ spec/features/requests/user_can_look_at_game_history_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'as an unregistered user' do
+  feature 'when they visit a game path' do
+    it 'shows them that games information' do
+      get "/api/v1/games/1"
+
+      game = JSON.parse(response.body)
+
+      expect(game).to eq(as_json)
+    end
+  end
+end
+
+def as_json
+  {
+    "game_id":1,
+    "scores": [
+      {
+        "user_id":1,
+        "score":15
+      },
+      {
+        "user_id":2,
+        "score":16
+      }
+    ]
+  }
+end
