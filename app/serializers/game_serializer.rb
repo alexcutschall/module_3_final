5cf12b1b1587159483706b8031df52b20a3df181
alexcutschall <alexander.cutschall@gmail.com>
2018-06-04 14:35:18 -0600

    scores have now been added together

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,6 +1,18 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :game_id
+  attributes :game_id, :player_1_score, :player_2_score
   has_many :scores
+
+  def player_1_score
+    object.player_1.plays.map do |play|
+      play.score
+    end.sum
+  end
+
+  def player_2_score
+    object.player_2.plays.map do |play|
+      play.score
+    end.sum
+  end
 
   class PlaySerializer < ActiveModel::Serializer
     attributes :user_id, :score
