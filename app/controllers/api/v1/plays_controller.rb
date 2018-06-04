f0d6d0481b6235702fdd3d438001d49fbd423834
alexcutschall <alexander.cutschall@gmail.com>
2018-06-04 14:15:58 -0600

    it now renders a 201 status along with the correct message

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class PlaysController < ApplicationController
+    class PlaysController < ApiController
       def create
         game = Game.find(params[:game_id])
         word = params[:word]
@@ -8,7 +8,8 @@ module Api
           WordSearch.new(params[:word]).validate
           game.plays.create(user_id: params[:player_id], word: word)
           game.save
-          render status: 201
+
+          render status: 201, json: ({"message": "'#{word}' is a valid word."})
         rescue
           render json: ({"message": "'#{word}' is not a valid word."})
         end
